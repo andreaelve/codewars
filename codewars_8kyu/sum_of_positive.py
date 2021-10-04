2d5b4ceb2bd6e081f205f74b3fdf605325e01c0d
Andrea Elvegård <andrea.elvegaard@yahoo.no>
2021-10-04 11:18:32 +0200

    legger til 5 oppgaver

---
+++ codewars_8kyu/sum_of_positive.py
@@ -0,0 +1,18 @@
+"""
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+
+"""
+
+def positive_sum(arr):
+    sum = 0
+    for num in arr:
+        if num > 0:
+            sum += num
+    return sum
+
+print(positive_sum([-1, 2, 3, 0, 7, -5]))
+
