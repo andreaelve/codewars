e85efe5c7d62464973ad1be6f64345fb5eafdf25
Salt Dev <saltdev@Salts-MacBook-Pro-22.local>
2022-05-09 19:07:52 +0200

    Ugly v2

--- codewars_6kyu/findMissingLetter.js
+++ codewars_6kyu/findMissingLetter.js
@@ -5,23 +5,24 @@
 // You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
 // The array will always contain letters in only one case.
 
-const findMissingLetter = (arr) => {
+function findMissingLetter(array){
     const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
     'M', 'N', 'O', 'P', 'Q', 'R',  'S', 'T', 'U', 'V', 'W', 'X',
     'Y', 'Z'];
     const lowerAlphabet = alphabet.map(el => el.toLowerCase());
-    let start = alphabet.findIndex(element => element == arr[0]);
-    if(start == -1){
-        start = lowerAlphabet.findIndex(element => element == arr[0]);
-        for(let char in arr){
-            if(arr[char] != lowerAlphabet[(start+parseInt(char, 10))]) return lowerAlphabet[(start+parseInt(char, 10))];
+    if(array){
+        let start = alphabet.findIndex(element => element == array[0]);
+        if(start == -1){
+            start = lowerAlphabet.findIndex(element => element == array[0]);
+            for(let char in array){
+                if(array[char] != lowerAlphabet[(start+parseInt(char, 10))]) return lowerAlphabet[(start+parseInt(char, 10))];
+            }
+        }
+        for(let char in array){
+            if(array[char] != alphabet[(start+parseInt(char, 10))]) return alphabet[(start+parseInt(char, 10))];
         }
     }
-    for(let char in arr){
-        if(arr[char] != alphabet[(start+parseInt(char, 10))]) return alphabet[(start+parseInt(char, 10))];
-    }
-
-    
+    return ' ';
 }
 console.log(findMissingLetter(['a','b','c','d','f']));
 console.log(findMissingLetter(['O','Q','R','S']));
