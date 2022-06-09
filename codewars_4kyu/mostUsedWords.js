86d5dac9773def4f2b04ff450c78cec44acaff80
Andrea Elvegård <andrea.elvegaard@yahoo.no>
2022-06-09 08:44:55 +0200

    Still in desperate need of some refactoring

--- codewars_4kyu/mostUsedWords.js
+++ codewars_4kyu/mostUsedWords.js
@@ -18,7 +18,7 @@
 // or an empty array if a text contains no words.
 
 const top_3_words = (string) => {
-  let words  = string.toLowerCase().replace(/[^a-z ']|\B'/g, '').replace(/^'$/g, '').split(' ').filter(word => word !== '');
+  let words  = string.toLowerCase().replace(/[^a-z ']|\B'/g, '').split(' ').filter(word => word !== '');
   let matchArray = [];
   words.forEach(el => {
     const subArray = words.filter(word => word == el);
@@ -33,9 +33,7 @@ const top_3_words = (string) => {
 
 console.log(top_3_words(`In a village of La Mancha, the name of which I have no desire to call to mind, there lived not long since one of those gentlemen that keep a lance in the lance-rack, an old buckler, a lean hack, and a greyhound for coursing. An olla of rather more beef than mutton, a salad on most
 nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
-on Sundays, made away with three-quarters of his income.`));
-// # => ["a", "of", "on"]
-
+on Sundays, made away with three-quarters of his income.`)); // # => ["a", "of", "on"]
 console.log(top_3_words("e e e e DDD ddd DdD: ddd ddd aa aA Aa, bb cc cC e e e")); // # => ["e", "ddd", "aa"]
 console.log(top_3_words("  //wont won't won't")); // # => ["won't", "wont"]
 console.log(top_3_words(`'`)); // # => [];
